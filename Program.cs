ea318910d8aad68a3006ac2eb739bd59ec6f004b
Pranjal Shrivastava <pranjals1994@hotmail.com>
2020-01-21 20:30:58 -0500

    New change

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is year 2020");
+        }
+    }
+}
